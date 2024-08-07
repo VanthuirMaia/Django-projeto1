46a3e386c5b68a26d726eda9f1bae3b152577b5b
Bowie <vanmaiasf@gmail.com>
2024-08-07 11:13:27 -0300

    templates e renderização

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request, 'recipes/home.html')
+
 
 def contato(request):
     return HttpResponse('CONTATO')
