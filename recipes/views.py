07cd858a2ba695a010ac11e981c6970eeafa1f5b
Bowie <vanmaiasf@gmail.com>
2024-08-08 13:08:10 -0300

    Criando head, usando o include para criar a partial

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Luiz Otavio',
     })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
