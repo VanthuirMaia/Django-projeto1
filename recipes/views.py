f5755f5d50255886ed5a2c9997d3d6f65131b1e1
Bowie <vanmaiasf@gmail.com>
2024-08-08 11:47:17 -0300

    Função render

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Luiz Otavio',
+    })
 
 
 def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'recipes/contato.html')
 
 def sobre(request):
     return HttpResponse('SOBRE')
